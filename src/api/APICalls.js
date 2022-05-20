91353bb30f7d562da22658808370add4da4beacc
bruceblack88 <bruceblack88@gmail.com>
2022-05-20 10:36:15 -0500

    updated / to api url and added async to api signature

--- src/api/APICalls.js
+++ src/api/APICalls.js
@@ -2,23 +2,23 @@ import {API_URL} from "../constants/Constants";
 import axios from "axios";
 
 
-export const apiGetIncident = (index) => {
+export const apiGetIncident = async (index) => {
     if (index > 0) {
-        return axios.get(API_URL + "incident" + {index})
+        return axios.get(API_URL + "incident/" + {index})
     } else {
         return axios.get(API_URL + "incident")
     }
 
 }
 
-export const apiPostIncident = (data) => {
+export const apiPostIncident = async (data) => {
     return axios.post(API_URL + "incident", data)
 }
 
-export const apiPatchIncident = (index, data) =>{
-    return axios.patch(API_URL + "incident" + {index}, data)
+export const apiPatchIncident = async (index, data) =>{
+    return axios.patch(API_URL + "incident/" + {index}, data)
 }
 
-export const apiDeleteIncident = (index) =>{
-    return axios.delete(API_URL + "incident" + {index})
+export const apiDeleteIncident = async (index) =>{
+    return axios.delete(API_URL + "incident/" + {index})
 }
