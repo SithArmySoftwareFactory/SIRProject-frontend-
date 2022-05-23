0ed68ca0de7da795edd0e063bb721125b7bd949a
Joshua Matos <josh@joshuamatos.com>
2022-05-23 16:18:49 -0500

    added data-grid, still needs to be worked on Component is SupervisorView under the components folder

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import {Banner} from "./components/common/Banner";
 import SIRForm from "./components/SIRForm";
 import Box from "@mui/material/Box";
+import SupervisorView from "./components/SupervisorView";
 
 function App() {
     return (
         <Box>
             <Banner/>
             <SIRForm/>
+
+            <SupervisorView />
+
         </Box>
     );
 }
