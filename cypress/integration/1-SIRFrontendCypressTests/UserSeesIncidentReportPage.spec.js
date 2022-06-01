c949503fbf5ef484b8fc76e4022f4fbae4d3627a
DakotaSlaySWF <dakota.g.slay.mil@swf.army.mil>
2022-06-01 14:41:33 -0500

    initial commit

---
+++ cypress/integration/1-SIRFrontendCypressTests/UserSeesIncidentReportPage.spec.js
@@ -0,0 +1,29 @@
+describe('user navigating to the homepage', () => {
+    beforeEach(() => {
+        cy.visit('/supervisor')
+    });
+    it('should see a banner and a menu button', () => {
+        cy.findByRole("banner").should('exist');
+        cy.findByRole("img", {name: /army software factory icon/i}).should('exist');
+    });
+
+    it('should see an Incident Reports header', () => {
+        cy.findByText(/incident reports/i).should('exist');
+    });
+
+    it('should see heading reports', () => {
+        cy.findByRole('cell',{name:/reports/i}).should('exist');
+    });
+
+    it('should see the filter button',()=>{
+       cy.findByRole('button',{name:/show filters/i}).should('exist');
+    });
+
+    it('should see a searchbox',()=>{
+        cy.findByRole('searchbox').should('exist');
+    })
+
+    it('should see a checkbox to select all boxes',()=>{
+        cy.findByRole('checkbox', {  name: /select all rows/i})
+    })
+});
