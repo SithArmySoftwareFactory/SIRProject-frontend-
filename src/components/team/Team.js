7268f3203819e5fab5a21d5af392e46b2b898ae5
lanceredfearn <lance.c.redfearn.mil@swf.army.mil>
2022-06-05 21:36:18 -0500

    cosmetic changes

--- src/components/team/Team.js
+++ src/components/team/Team.js
@@ -1,32 +1,36 @@
 import React from 'react';
 import {Grid, Typography} from "@mui/material";
 import Card from "@mui/material/Card";
+import Paper from "@mui/material/Paper";
 
 const Team = () => {
     return (
         <Grid container spacing={5} display={"flex"} justifyContent={"center"}>
             <Grid item xs={2}>
-                <Card>
+                <Paper variant={"outlined"} elevation={24} sx={{border: "1px solid black"}}>
+                    ANYTHING
+                </Paper>
+                <Card elevation={24}>
                     <img src={require("./Redfearn1.jpeg")} alt="Lance Redfearn"/>
                 </Card>
             </Grid>
             <Grid item xs={2}>
-                <Card>
+                <Card elevation={24}>
                     <img src={require("./Matos1.jpeg")} alt="Joshua Matos" height={487} width={308}/>
                 </Card>
             </Grid>
             <Grid item xs={2}>
-                <Card>
+                <Card elevation={24}>
                     <img src={require("./Slay1.jpeg")} alt="Dakota Slay" height={487} width={308}/>
                 </Card>
             </Grid>
             <Grid item xs={2}>
-                <Card>
+                <Card elevation={24}>
                     <img src={require("./Redfearn1.jpeg")} alt="Lance Redfearn"/>
                 </Card>
             </Grid>
             <Grid item xs={2}>
-                <Card>
+                <Card elevation={24}>
                     <img src={require("./Redfearn1.jpeg")} alt="Lance Redfearn"/>
                 </Card>
             </Grid>
