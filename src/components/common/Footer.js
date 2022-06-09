7f50fc79ab82a9e3e4271af19a8446157e90c901
bruceblack88 <bruceblack88@gmail.com>
2022-06-09 12:18:05 -0500

    fixed links openening to new tab

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -17,19 +17,19 @@ const Footer = () => {
                                     onClick={() => AnalyticsEventTracker('External Links', 'MEDVAC Link', 'clicked')}>MEDVAC
                                     APP</a></li>
                                 <li><a href="https://dev3395.d20mm6b3fjxaop.amplifyapp.com/"
-                                       onClick={() => AnalyticsEventTracker('External Links', 'Incubator Link', 'clicked')}>The INCUBATOR</a></li>
+                                       onClick={() => AnalyticsEventTracker('External Links', 'Incubator Link', 'clicked')} target="_blank">The INCUBATOR</a></li>
                                 <li><a href="#"
-                                       onClick={() => AnalyticsEventTracker('External Links', 'SIR Link', 'clicked')}>SIR REPORT</a></li>
+                                       onClick={() => AnalyticsEventTracker('External Links', 'SIR Link', 'clicked')} target="_blank">SIR REPORT</a></li>
                             </ul>
                         </div>
                         <div className="col-sm-6 col-md-3 item">
                             <h3>About</h3>
                             <ul>
                                 <li><a href="https://armyfuturescommand.com/software-factory/"
-                                       onClick={() => AnalyticsEventTracker('Software Factory', 'Clicked Company button', 'clicked')}>Company</a></li>
+                                       onClick={() => AnalyticsEventTracker('Software Factory', 'Clicked Company button', 'clicked')} target="_blank">Company</a></li>
                                 <li><NavLink page="/team" to="/team">Team</NavLink></li>
                                 <li><a href="https://armyfuturescommand.com/software-factory-2/"
-                                       onClick={() => AnalyticsEventTracker('Software Factory', 'Clicked Careers Button', 'Routed to SWF Application')}>Careers</a></li>
+                                       onClick={() => AnalyticsEventTracker('Software Factory', 'Clicked Careers Button', 'Routed to SWF Application')} target="_blank">Careers</a></li>
                             </ul>
                         </div>
                         <div className="col-md-6 item text">
